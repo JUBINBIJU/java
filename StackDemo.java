e4ea67dc0fb79da5e1e33b3768db1c3381128e45
sreejith19 <56436219+sreejith19@users.noreply.github.com>
2020-03-12 11:24:09 +0530

    Update StackDemo.java

--- StackDemo.java
+++ StackDemo.java
@@ -1,3 +1,48 @@
+class Stack
+{
+	private int MAX,top=-1;
+	private int[] stack;
+	public Stack()
+	{
+		this(20);
+	}
+	public Stack(int a)
+	{
+		MAX=a;
+		stack = new int[a];
+	}
+	public void print(String s)
+	{
+		System.out.println(s);
+	}
+	public void push()
+	{
+		push(0);
+	}
+	public void push(int item)
+	{
+		if(top<MAX-1)
+		{
+			stack[++top]=item;
+		}
+		else
+		{
+			System.out.println("STACK OVERFLOW!!!");
+		}
+	}
+	public int pop()
+	{
+		if(top>=0)
+		{
+			return stack[top--];
+		}
+		else
+		{
+			System.out.println("STACK UNDERFLOW!!!");
+			return -100000;
+		}
+	}
+}
 
 public class StackDemo
 {
@@ -6,9 +51,9 @@ public class StackDemo
 		Stack s=new Stack();
 		s.push(5);
 		s.push(8);
-		so.p(s.pop());
+		System.out.println(s.pop());
 		s.push(4);
-		so.p(s.pop()+s.pop());
-		so.p(s.pop());
+		System.out.println(s.pop()+s.pop());
+		System.out.println(s.pop());
 	}
 }
