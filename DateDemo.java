c9ab62adb94167e69e76449d105f801d1cd8450d
sreejith19 <56436219+sreejith19@users.noreply.github.com>
2020-03-12 11:09:26 +0530

    Add files via upload

---
+++ DateDemo.java
@@ -0,0 +1,99 @@
+
+
+class Date{
+
+	private int date,month;
+	/*private String[] months = { "January" , "February" , "March" , "April" , "May" , "June" , "July"  , "August" , "September" ,  "October" ,"November" ,"December"};*/
+	private int days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	Date()
+	{
+		this(1,1);
+	}
+
+	Date(int d, int m)
+	{
+		date=d;
+		month=m;
+	}
+	/*public int daysInMonth()
+	{
+		for(int i=0;i<12;i++)
+		{
+			System.out.println(months[i]+" : "+days[i]);
+		}
+		return -1;
+	}*/
+
+	public int daysInMonth(int x)
+	{
+		return ((x>0&&x<13)?days[x-1]:-1);
+	}
+
+	public int daysInMonth()
+	{
+		return ((month>0&&month<13)?days[month-1]:-1);
+	}
+
+	public int getDay()
+	{
+		return date;
+	}
+
+	public int getMonth()
+	{
+		return month;
+	}
+
+	void nextDay()
+	{
+		date=date%days[month-1]+1;
+		if(date==1)
+		{
+			month=month%12+1;
+		}
+	}
+
+	public String toString()
+	{
+		String s=" ";
+		if(date<10)
+		{
+			s+="0";
+		}
+		s+=date+"/";
+		if(month<10)
+		{
+			s+="0";
+		}
+		s+=month;
+		return s;
+	}
+
+	int absoluteDay()
+	{
+		int a=date;
+		for(int i=0;i<month-1;i++)
+		{
+			a+=days[i];
+		}
+		return a;
+	}
+}
+
+public class DateDemo{
+	public static void main(String[] args)
+	{
+		Date jan1 = new Date(1,1);
+		for(int i=0;i<367;i++)
+		{
+			so.pr(jan1);
+			so.p("    " + jan1.absoluteDay());
+			jan1.nextDay();
+		}
+		so.p(jan1.daysInMonth());
+		so.p(jan1.daysInMonth(4));
+		so.p(jan1.getMonth());
+		so.p(jan1.getDay());
+	}
+}
